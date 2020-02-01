e476fa1d91626c4d631c06a7304b0ea0847b4950
Hailey Bongo <haileyebongo@gmail.com>
2020-02-01 02:04:52 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
   lowest_value = 0
   lowest_key = nil
- name_hash.each do |name, number|
+ name_hash.each do |key,value|
     if number > lowest
       lowest = name 
     else
