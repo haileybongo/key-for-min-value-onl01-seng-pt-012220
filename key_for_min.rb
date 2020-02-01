d113da40ce26c3da30b6bbbd2d783f0d553ae854
Hailey Bongo <haileyebongo@gmail.com>
2020-02-01 01:52:35 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
- name_hah.collect do |kids_name, age|
+ name_hah.collect do |item, value|
     age
 end
 
